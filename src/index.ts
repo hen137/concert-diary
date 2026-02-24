d7c153ce274879cffde8f47c2d8910656036590d
Henry Abramovich <henry.abramovich@gmail.com>
2026-02-23 21:59:13 -0500

    decoupled server logic

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { buildServer } from "./server.js";
+
+const server = await buildServer({})
+
+await server.listen({
+    port: server.env.PORT,
+    host: '0.0.0.0',
+    listenTextResolver: (address) => `Server listening on ${address}`
+})
+    .then(() => {
+
+    })
+    .catch((error) => {
+        console.error('Error starting server:', error);
+        process.exit(1);
+    });
