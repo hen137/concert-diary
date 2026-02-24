b7b2acdde82083a3f456df24ece237998842a7c1
Henry Abramovich <henry.abramovich@gmail.com>
2026-02-24 00:33:15 -0500

    fixed ref to global test helpers

--- tests/jest.config.ts
+++ tests/jest.config.ts
@@ -3,9 +3,9 @@ import type { Config } from 'jest';
 const config: Config = {
   // ... other Jest configurations
   // A global setup script that runs once before all tests
-  globalSetup: '<rootDir>/tests/global-setup.ts',
+  globalSetup: '<rootDir>/utils/global-setup.ts',
   // A global teardown script that runs once after all tests
-  globalTeardown: '<rootDir>/tests/global-teardown.ts',
+  globalTeardown: '<rootDir>/utils/global-teardown.ts',
 };
 
 export default config;
