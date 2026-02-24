e47e99c1334cda8a42afd29d7906ef266546aa77
Henry <henry.abramovich@gmail.com>
2026-02-23 22:36:15 -0500

    changed schema name

--- src/routes/v1/users/_id/routes.ts
+++ src/routes/v1/users/_id/routes.ts
@@ -1,9 +1,9 @@
 import type { FastifyInstance } from "fastify";
-import { getUserByIdSchema } from "../schemas.js";
+import { getUserSchema } from "../schemas.js";
 import { deactivateUser, getUser, updateUser } from "../controllers.js";
  
 export default async function (server: FastifyInstance) {
-    server.get('', { schema: getUserByIdSchema }, getUser)
+    server.get('', { schema: getUserSchema }, getUser)
     server.put('', {}, updateUser)
     server.delete('', {}, deactivateUser)
 }
