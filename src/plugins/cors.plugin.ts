2671df4a06e7293d40bb64923d6f0e4c13b5da63
Henry Abramovich <henry.abramovich@gmail.com>
2026-03-06 02:11:55 -0500

    added cors plugin

---
+++ src/plugins/cors.plugin.ts
@@ -0,0 +1,21 @@
+import type { FastifyPluginOptions } from 'fastify';
+import type { Server } from '../index.js';
+
+import cors from '@fastify/cors';
+import fp from 'fastify-plugin';
+
+async function corsPlugin(server: Server, options: FastifyPluginOptions) {
+    try {
+        await server.register(cors, { // Fastify CORS options: https://github.com/fastify/fastify-cors?tab=readme-ov-file#options
+            // TODO: configure CORS options for production, dev environments
+        })
+
+        server.log.debug('CORS plugin registered successfully');
+    } catch (error) {
+        server.log.error(error, 'Error registering CORS plugin:');
+    }
+}
+
+export default fp(corsPlugin, {
+    name: 'cors',
+})
