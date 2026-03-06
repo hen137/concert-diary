fa0b488a85c20d87f570f529a4f5328835b9c620
Henry Abramovich <henry.abramovich@gmail.com>
2026-03-06 02:18:07 -0500

    relocated server options

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
 import { buildServer } from "./server.js";
 
-const server = buildServer({})
+const server = buildServer({
+    // TODO: expand logging configuration
+    logger: {
+        level: 'trace',
+        transport: {
+            target: 'pino-pretty',
+            options: {
+                colorize: true,
+                translateTime: 'HH:MM:ss Z',
+            }
+        },
+    },
+    genReqId(_req) { // generates a random 10 character string ensuring unique request ids
+        return Math.random().toString(36).substring(2, 12);
+    },
+})
 
 // TODO: improve server type inference
 export type Server = typeof server;
--- src/server.ts
+++ src/server.ts
@@ -7,24 +7,10 @@ import Fastify from 'fastify';
 import autoLoad from '@fastify/autoload';
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
 
+// Fastify server factory function 
 export function buildServer(options: FastifyServerOptions) {
     // the server object, modifed to use the ZodTypeProvider for schema validation, serialization and type inference in routes
     const server = Fastify({
-        // TODO: expand logging configuration
-        logger: {
-            level: 'trace',
-            transport: {
-                target: 'pino-pretty',
-                options: {
-                    colorize: true,
-                    translateTime: 'HH:MM:ss Z',
-                }
-            },
-        },
-        // generates a random 10 character string ensuring unique request ids
-        genReqId(_req) {
-            return Math.random().toString(36).substring(2, 12);
-        },
         ...options
     }).withTypeProvider<ZodTypeProvider>();
 
