89ff00f301645e85550664c42c2d738b788cf393
Henry Abramovich <henry.abramovich@gmail.com>
2026-03-05 19:15:02 -0500

    moved test endpoints

---
+++ src/routes/tests/routes.ts
@@ -0,0 +1,17 @@
+import type { Server } from "../../index.js";
+
+import { testSchema } from "./schemas.js";
+import { logger } from '../../utils/logger.utils.js';
+import { ALS } from "../../utils/als.utils.js";
+
+export default async function userRoutes(server: Server) {
+    // NOTE: conisder registering route hooks
+
+    server.get('', { schema: testSchema }, async (request, reply) => {
+        logger.info('Received request with query:');
+
+        return {
+            foo: 'bar'
+        }
+    })
+}
--- src/routes/tests/schemas.ts
+++ src/routes/tests/schemas.ts
@@ -4,27 +4,24 @@ export const testSchema = {
     summary: 'Template Schema',
     description: 'Testbed for API schema definitions.',
     tags: ['tests'],
-    // params: z.object({
+    params: z.object({
 
-    // }),
-    querystring: z.object({
-        flag: z.stringbool()
     }),
-    // headers: z.object({
+    querystring: z.object({
+        // flag: z.stringbool()
+    }),
+    headers: z.object({
 
-    // }),
+    }),
     response: {
         200: z.object({
             foo: z.string()
         }).meta({ description: "200 success response for the test handler" }),
-        201: z.object({
-            test: z.string()
-        }),
         500: z.object({
             "statusCode": z.int(),
             "code": z.string(),
             "error": z.string(),
             "message": z.string()
-        })
+        }).meta({ description: "500 error response for the test handler" })
     }
 }
