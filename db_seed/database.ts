30887a077345dbecaea6994420423d490b8393c8
Henry <henry.abramovich@gmail.com>
2026-03-20 01:08:59 -0400

    weakly decoupled pg pool object

--- db_seed/database.ts
+++ db_seed/database.ts
@@ -1,19 +1,21 @@
 import type { DB } from "../src/types/database.js";
 
-import { Pool } from 'pg';
-import { Kysely, PostgresDialect } from 'kysely';
+import { Pool } from "pg";
+import { Kysely, PostgresDialect } from "kysely";
 
-const dialect =  new PostgresDialect({
-    pool: new Pool({
-        host: process.env.POSTGRES_HOST,
-        port: process.env.POSTGRES_PORT ? parseInt(process.env.POSTGRES_PORT, 10) : 5432,
-        user: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD,
-        database: process.env.POSTGRES_DB
-    })
-})
+const pool = new Pool({
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT
+    ? parseInt(process.env.POSTGRES_PORT, 10)
+    : 5432,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+});
 
 export const db = new Kysely<DB>({
-    dialect,
-    plugins: [],
-})
+  dialect: new PostgresDialect({
+    pool: pool,
+  }),
+  plugins: [],
+});
