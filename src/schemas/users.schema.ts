ee1ac200a01300614e9105cf158666bdd0bc3b03
Henry <henry.abramovich@gmail.com>
2026-03-28 15:35:44 -0400

    elaborated user route schemas

--- src/schemas/users.schema.ts
+++ src/schemas/users.schema.ts
@@ -2,12 +2,15 @@ import { z } from "zod"
 import { paginationSchema } from "./snippets/pagination.schema.js";
 
 const userSchema = z.object({
-    "id": z.number(),
-    "name": z.string(), // NOTE: consider including username 
-    "profile_url": z.string(), // TODO: change to URL type when supported
-    "avatar_url": z.string(), // TODO: change to URL type when supported
-    "api_link": z.string(), // TODO: change to URL type when supported
-})
+  id: z.number(),
+  username: z.string(),
+  full_name: z.string(),
+  followers_count: z.int(),
+  following_count: z.int(),
+  avatar_url: z.string(), // TODO: change to URL type when supported
+  api_link: z.string(), // TODO: change to URL type when supported
+  created_at: z.string(),
+});
 
 export const getUserListSchema = {
     summary: 'Get list of users',
@@ -49,34 +52,39 @@ export const getUserSchema = {
 
     }),
     response: {
-        200: z.object({
-            "statusCode": z.int(),
-            "id": z.number(),
-            "name": z.string(), // NOTE: consider including username 
-            "profile_url": z.string(), // TODO: change to URL type when supported
-            "avatar_url": z.string(), // TODO: change to URL type when supported
-            "followers": z.int(),
-            "following": z.int(),
-            "followers_url": z.string(), // TODO: change to URL type when supported
-            "following_url": z.string(), // TODO: change to URL type when supported
-            "reviews_url": z.string(), // TODO: change to URL type when supported
-        }).meta({ description: "200 success response for the get user handler" }),
+    200: userSchema
+      .extend({
+        statusCode: z.int(),
+        followers_url: z.string(), // TODO: change to URL type when supported
+        following_url: z.string(), // TODO: change to URL type when supported
+        reviews_url: z.string(), // TODO: change to URL type when supported
+      })
+      .meta({ description: "200 success response for the get user handler" }),
+    404: z
+      .object({
+        statusCode: z.int(),
+      })
+      .meta({ description: "404 not found response for the get user handler" }),
         // 500: serverErrorSchema
     }
 }
 
 export const createUserSchema = {
-    summary: 'Create a user',
-    description: 'Creates a new user.',
-    tags: ['users'],
-    params: z.object({
-
-    }),
-    querystring: z.object({
-        
-    }),
-    headers: z.object({
-
+  summary: "Create a user",
+  description: "Creates a new user.",
+  tags: ["users"],
+  params: z.object({}),
+  querystring: z.object({}),
+  headers: z.object({}),
+  body: z.object({
+    username: z.string(),
+    email: z.string(),
+    password_hash: z.string(),
+    first_name: z.string(),
+    last_name: z.string(),
+    gender: z.string(),
+    date_of_birth: z.date(),
+    avatar_url: z.string(),
     }),
     response: {
         200: z.object({
