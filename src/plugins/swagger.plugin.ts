46b6387c009239990323c20fefbc021d603eb12e
Henry <henry.abramovich@gmail.com>
2026-03-31 00:24:47 -0400

    updates to swagger plugin options

--- src/plugins/swagger.plugin.ts
+++ src/plugins/swagger.plugin.ts
@@ -8,43 +8,51 @@ import { jsonSchemaTransform } from 'fastify-type-provider-zod';
 
 async function swaggerPlugin(server: Server, options: FastifyPluginOptions) {
     try {
-        await server.register(swagger, { // Swagger options: https://github.com/fastify/fastify-swagger?tab=readme-ov-file#options
-            transform: jsonSchemaTransform, // make swagger support Zod schemas
-            openapi: { // OpenAPI options: https://swagger.io/specification/#oasObject
-                // TODO: complete spec
-                openapi: '3.1.1',
-                info: {
-                    title: 'Concert Diary API',
-                    description: 'API documentation for the Concert Diary application',
-                    version: '0.1.0',
-                },
-                servers: [
-                    {
-                        url: `http://localhost:3000`,
-                        description: 'Dev Server',
-                    },
-                ],
-                tags: [
-                    // TODO: add more primary entity tags
-                    { name: 'tests', description: 'Testing and experimental APIs' },
-                    { name: 'users', description: 'User management APIs' },
-                ],
-                components: {
-                    // securitySchemes: {
-                    //   bearerAuth: {
-                    //     type: 'http',
-                    //     scheme: 'bearer',
-                    //     bearerFormat: 'JWT',
-                    //   },
-                    // },
-                },
-                security: [
-                    // {
-                    //   bearerAuth: [],
-                    // },
-                ],
+        await server.register(swagger, {
+          // Swagger options: https://github.com/fastify/fastify-swagger?tab=readme-ov-file#options
+          transform: jsonSchemaTransform, // make swagger support Zod schemas
+          openapi: {
+            // OpenAPI options: https://swagger.io/specification/#oasObject
+            // TODO: complete spec
+            openapi: "3.1.1",
+            info: {
+              title: "Concert Diary API",
+              description:
+                "API documentation for the Concert Diary application",
+              version: "0.1.0",
             },
-        })
+            servers: [
+              {
+                url: `http://localhost:3000`,
+                description: "Dev Server",
+              },
+            ],
+            tags: [
+              // TODO: add more primary entity tags
+              { name: "tests", description: "Testing and experimental APIs" },
+              { name: "users", description: "User management APIs" },
+              { name: "events", description: "Event management APIs" },
+              { name: "artists", description: "Artist management APIs" },
+              { name: "venues", description: "Venue management APIs" },
+              { name: "series", description: "Series management APIs" },
+              { name: "reviews", description: "Review management APIs" },
+            ],
+            components: {
+              securitySchemes: {
+                bearerAuth: {
+                  type: 'http',
+                  scheme: 'bearer',
+                  bearerFormat: 'JWT',
+                },
+              },
+            },
+            security: [
+              {
+                bearerAuth: [],
+              },
+            ],
+          },
+        });
 
         await server.register(swaggerUi, { // Swagger UI options: https://github.com/fastify/fastify-swagger-ui?tab=readme-ov-file#api
             routePrefix: '/docs',
