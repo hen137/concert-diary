dc255d070aa5cf123c7dc4558a49b651482bfb8c
Henry <henry.abramovich@gmail.com>
2026-04-08 20:36:52 -0400

    emptied test routes

--- src/routes/tests/_foo/index.ts
+++ src/routes/tests/_foo/index.ts
@@ -2,16 +2,10 @@ import type { Server } from "../../../index.js";
 
 import { testParamSchema } from "../../../schemas/tests.schemas.js";
 import { logger } from "../../../utils/logger.utils.js";
+import { session } from "../../../utils/session.utils.js";
 
 export default async function testRoutes(server: Server) {
-  // NOTE: conisder registering route hooks
-
   server.get("", { schema: testParamSchema }, async (request, response) => {
-    return {
-      statusCode: response.statusCode,
-      params: request.params,
-      // "query": request.query,
-      // "headers": request.headers
-    };
+    // logger.trace({ session: session.getSession() }, "GET /tests/{foo}");
   });
 }
