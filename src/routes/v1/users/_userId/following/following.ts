e60605ce4dc85f4c6a6745129779b3059588a108
Henry <henry.abramovich@gmail.com>
2026-04-27 15:52:03 -0400

    refactor(user routes): Add pagincation support

---
+++ src/routes/v1/users/_userId/following/following.ts
@@ -0,0 +1,138 @@
+import type {
+  IUserData,
+  IUserCursor,
+} from '../../../../../types/query_data.js';
+import type { Server } from '../../../../../index.js';
+
+import z from 'zod';
+import { getFollowingSchema } from '../../../../../schemas/users.schema.js';
+import { userCursorSchema } from '../../../../../schemas/snippets/pagination.schema.js';
+import { logger } from '../../../../../utils/logger.utils.js';
+import {
+  formatUsersPayload,
+  validateCursorLimit,
+} from '../../../../../utils/pagination.utils.js';
+import {
+  dataNotFoundResponse,
+  getPaginationErrorResponse,
+} from '../../../../../utils/error_responses.utils.js';
+
+type ErrorResponse =
+  | z.infer<(typeof getFollowingSchema.response)[400]>
+  | z.infer<(typeof getFollowingSchema.response)[500]>;
+
+type NotFoundResponse = z.infer<(typeof getFollowingSchema.response)[404]>;
+
+export default async function (server: Server) {
+  server.setErrorHandler((error, request, response): ErrorResponse => {
+    if (!(process.env.NODE_ENV === 'test')) logger.error(error);
+
+    const { code, errorResponse } = getPaginationErrorResponse(error);
+
+    response.code(code);
+    return errorResponse;
+  });
+
+  server.setNotFoundHandler((request, response): NotFoundResponse => {
+    response.code(404);
+    return dataNotFoundResponse;
+  });
+
+  server.get('', { schema: getFollowingSchema }, async (request, response) => {
+    // CONSIDER: add support for offset/page pagination
+    // TODO: implement sorting and filtering
+    // TODO: add support for first and last page cursors
+
+    //extract query params
+    const { cursor, limit } = validateCursorLimit(
+      request.query.cursor,
+      request.query.limit,
+      userCursorSchema
+    );
+    const { userId } = request.params;
+
+    // data variables
+    let userData: IUserData[];
+    let prevUserData: IUserCursor[] = [];
+
+    // build query
+    const query = server.db
+      .selectFrom('user_relationships')
+      .innerJoin(
+        (eb) =>
+          eb
+            .selectFrom('user_accounts')
+            .innerJoin(
+              'user_profiles',
+              'user_accounts.user_id',
+              'user_profiles.user_id'
+            )
+            .select([
+              'user_accounts.user_id',
+              'user_accounts.created_at',
+              'user_accounts.username',
+              'user_profiles.first_name',
+              'user_profiles.last_name',
+              'user_profiles.avatar_url',
+            ])
+            .as('users'),
+        (join) =>
+          join.onRef('user_relationships.target_id', '=', 'users.user_id')
+      )
+      .selectAll('users')
+      .where('user_relationships.initiator_id', '=', userId)
+      .orderBy('users.created_at', 'desc')
+      .orderBy('users.user_id', 'desc')
+      .limit(limit + 1);
+
+    if (cursor) {
+      const {
+        userId,
+        // createdAt
+      } = cursor;
+
+      userData = await query
+        .where('users.user_id', '<', userId)
+        // .where('users.created_at', '<', createdAt)
+        .execute();
+
+      // TODO: validate userId exists
+      // validate existance of supplied userId
+      // logger.debug(userData.length)
+      if (!userData.length) {
+        return response.callNotFound();
+      }
+
+      prevUserData = await server.db
+        .selectFrom('user_relationships')
+        .innerJoin(
+          (eb) =>
+            eb
+              .selectFrom('user_accounts')
+              .select(['user_accounts.user_id', 'user_accounts.created_at'])
+              .as('users'),
+          (join) =>
+            join.onRef('user_relationships.target_id', '=', 'users.user_id')
+        )
+        .selectAll('users')
+        .where('user_relationships.initiator_id', '=', userId)
+        // .where('users.created_at', '>', createdAt)
+        .where('users.user_id', '>', userId)
+        .orderBy('users.created_at', 'asc')
+        .orderBy('users.user_id', 'asc')
+        .limit(limit)
+        .execute();
+
+      if (!prevUserData)
+        throw new Error(
+          'Data integrity error: cursor points to non-existent data'
+        );
+    } else {
+      userData = await query.execute();
+    }
+
+    const payload = formatUsersPayload(cursor, limit, userData, prevUserData);
+
+    return payload;
+  });
+}
--- src/routes/v1/users/users.ts
+++ src/routes/v1/users/users.ts
@@ -1,173 +1,111 @@
-import type { Server } from '../../../index.js'
+import type { IUserData, IUserCursor } from '../../../types/query_data.js';
+import type { Server } from '../../../index.js';
 
-import { getUserListSchema } from '../../../schemas/users.schema.js'
+import z from 'zod';
+import { getUserListSchema } from '../../../schemas/users.schema.js';
+import { userCursorSchema } from '../../../schemas/snippets/pagination.schema.js';
+import { logger } from '../../../utils/logger.utils.js';
 import {
-  getUserListErrorHandler,
-  getUserListHandler,
-  getUserListNotFoundHandler,
-} from '../../../handlers/users.handlers.js'
+  formatUsersPayload,
+  validateCursorLimit,
+} from '../../../utils/pagination.utils.js';
+import {
+  getPaginationErrorResponse,
+  dataNotFoundResponse,
+} from '../../../utils/error_responses.utils.js';
+
+type ErrorResponse =
+  | z.infer<(typeof getUserListSchema.response)[400]>
+  | z.infer<(typeof getUserListSchema.response)[500]>;
+
+type NotFoundResponse = z.infer<(typeof getUserListSchema.response)[404]>;
 
 export default async function usersRoutes(server: Server) {
-  server.setErrorHandler(getUserListErrorHandler)
-  server.setNotFoundHandler(getUserListNotFoundHandler)
+  server.setErrorHandler((error, request, response): ErrorResponse => {
+    if (!(process.env.NODE_ENV === 'test')) logger.error(error);
 
-  server.get('', { schema: getUserListSchema }, getUserListHandler)
+    const { code, errorResponse } = getPaginationErrorResponse(error);
 
-  // server.setErrorHandler((error, request, response) => {
-  //   logger.error(error)
-  //   if (
-  //     error instanceof DOMException || // invalid base64 encoding of cursor
-  //     error instanceof SyntaxError || // invalid JSON from decoded cursor
-  //     error instanceof z.ZodError // invalid object members in decoded cursor
-  //   ) {
-  //     response.code(400)
-  //     return {
-  //       error: 'Bad request',
-  //       message: 'Malformed cursor',
-  //     }
-  //   }
-  //   // else if (error instanceof ERROR) {
-  //   //   response.code(number)
-  //   //   return {}
-  //   // }
-  //   else {
-  //     response.code(500)
-  //     return {
-  //       error: 'Internal Server Error',
-  //     }
-  //   }
-  // })
+    response.code(code);
+    return errorResponse;
+  });
 
-  // server.setNotFoundHandler((request, response) => {
-  //   response.code(404)
-  //   return {
-  //     error: 'Not found',
-  //     message: 'User does not exist',
-  //   }
-  // })
+  server.setNotFoundHandler((request, response): NotFoundResponse => {
+    response.code(404);
+    return dataNotFoundResponse;
+  });
 
-  //   server.get('', { schema: getUserListSchema }, async (request, response) => {
-  //     // CONSIDER: add support for offset/page pagination
-  //     // TODO: implement sorting and filtering
-  //     // TODO: add support for first and last page cursors
+  server.get('', { schema: getUserListSchema }, async (request, response) => {
+    // CONSIDER: add support for offset/page pagination
+    // TODO: implement sorting and filtering
+    // TODO: add support for first and last page cursors
 
-  //     // extract query params
-  //     const { cursor } = request.query
-  //     const limit = Math.min(Math.max(request.query.limit, 1), 100)
+    // extract query params
+    const { cursor, limit } = validateCursorLimit(
+      request.query.cursor,
+      request.query.limit,
+      userCursorSchema
+    );
 
-  //     // data variables
-  //     let accountData: {
-  //       user_id: string
-  //       created_at: Date
-  //       username: string
-  //       avatar_url: string | null
-  //       first_name: string
-  //       last_name: string
-  //     }[]
-  //     let prevAccountData: { user_id: string; created_at: Date }[] = []
+    // data variables
+    let userData: IUserData[];
+    let prevUserData: IUserCursor[] = [];
 
-  //     // build query
-  //     const query = server.db
-  //       .selectFrom('user_accounts')
-  //       .innerJoin(
-  //         'user_profiles',
-  //         'user_accounts.user_id',
-  //         'user_profiles.user_id'
-  //       )
-  //       .select([
-  //         'user_accounts.user_id',
-  //         'user_accounts.created_at',
-  //         'user_accounts.username',
-  //         'user_profiles.first_name',
-  //         'user_profiles.last_name',
-  //         'user_profiles.avatar_url',
-  //       ])
-  //       .orderBy('user_accounts.created_at', 'desc')
-  //       .orderBy('user_accounts.user_id', 'desc')
-  //       .limit(limit + 1)
+    // build query
+    const query = server.db
+      .selectFrom('user_accounts')
+      .innerJoin(
+        'user_profiles',
+        'user_accounts.user_id',
+        'user_profiles.user_id'
+      )
+      .select([
+        'user_accounts.user_id',
+        'user_accounts.created_at',
+        'user_accounts.username',
+        'user_profiles.first_name',
+        'user_profiles.last_name',
+        'user_profiles.avatar_url',
+      ])
+      .limit(limit + 1);
 
-  //     if (cursor) {
-  //       // decode cursor
-  //       const cursorDecoded = JSON.parse(atob(cursor))
+    if (cursor) {
+      const {
+        userId,
+        // createdAt,
+      } = cursor;
 
-  //       // validate cursor contents
-  //       const { userId, createdAt } = cursorSchema.parse(cursorDecoded)
+      // OPTIMIZE: retireve this cursor and previous cursor in a single query
+      userData = await query
+        .where('user_accounts.user_id', '<', userId)
+        // .where('user_accounts.created_at', '<', createdAt)
+        .orderBy('user_accounts.created_at', 'desc')
+        .orderBy('user_accounts.user_id', 'desc')
+        .execute();
 
-  //       // OPTIMIZE: retireve this cursor and previous cursor in a single query
-  //       // query data within cursor window
-  //       accountData = await query
-  //         .where('user_accounts.created_at', '<', createdAt)
-  //         .where('user_accounts.user_id', '<', userId)
-  //         .execute()
+      // no records following userId
+      if (!userData.length) return response.callNotFound();
 
-  //       //  TODO: validate userId exists
-  //       // validate existance of supplied userId
-  //       // logger.debug(accountData.length)
-  //       // if (!accountData.length) {
-  //       //   return response.callNotFound()
-  //       // }
+      prevUserData = await server.db
+        .selectFrom('user_accounts')
+        .select(['user_id', 'created_at'])
+        .where('user_id', '>', userId)
+        // .where('created_at', '>', createdAt)
+        .orderBy('created_at', 'asc')
+        .orderBy('user_id', 'asc')
+        .limit(limit + 1)
+        .execute();
 
-  //       // FIX: see optimize above
-  //       prevAccountData = await server.db
-  //         .selectFrom('user_accounts')
-  //         .select(['user_id', 'created_at'])
-  //         .where('created_at', '>', createdAt)
-  //         .where('user_id', '>', userId)
-  //         .orderBy('created_at', 'asc')
-  //         .orderBy('user_id', 'asc')
-  //         .limit(limit)
-  //         .execute()
-  //     } else {
-  //       accountData = await query.execute()
-  //     }
+      // if (!prevUserData)
+      // throw new Error(
+      //   'Data integrity error: cursor points to non-existent data'
+      // );
+    } else {
+      userData = await query.execute();
+    }
 
-  //     // format data
-  //     let data = []
-  //     for (const account of accountData) {
-  //       data.push({
-  //         user_id: account.user_id,
-  //         username: account.username,
-  //         full_name: `${account.first_name} ${account.last_name}`,
-  //         // TODO: calculate followers/following count
-  //         followers_count: 0,
-  //         following_count: 0,
-  //         avatar_url: account.avatar_url ?? '',
-  //         api_path: `${server.prefix}/${account.user_id}`,
-  //         created_at: account.created_at,
-  //       })
-  //     }
+    const payload = formatUsersPayload(cursor, limit, userData, prevUserData);
 
-  //     // format next cursor
-  //     const next_cursor =
-  //       data.length > limit
-  //         ? btoa(
-  //             JSON.stringify({
-  //               userId: data[data.length - 1]!.user_id,
-  //               createdAt: data[data.length - 1]!.created_at,
-  //             })
-  //           )
-  //         : ''
-
-  //     data = data.slice(0, limit) // trim to limit
-
-  //     // format previous cursor
-  //     const previous_cursor = prevAccountData
-  //       ? btoa(
-  //           JSON.stringify({
-  //             userId: prevAccountData[prevAccountData.length - 1]?.user_id,
-  //             createdAt: prevAccountData[prevAccountData.length - 1]?.created_at,
-  //           })
-  //         )
-  //       : ''
-
-  //     return {
-  //       page: {
-  //         limit: data.length,
-  //         cursor,
-  //         next_cursor,
-  //         previous_cursor,
-  //       },
-  //       data,
-  //     }
-  //   })
+    return payload;
+  });
 }
