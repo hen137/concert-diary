e93db860c418b5cee7a6e584d1ad82b135f37e97
Henry <henry.abramovich@gmail.com>
2026-05-01 15:04:15 -0400

    perf(test): Migrate to vitest

--- tests/utils/global_setup.ts
+++ tests/utils/global_setup.ts
@@ -1,9 +1,9 @@
-import fs from "fs/promises";
+import fs from 'fs/promises';
 import {
   PostgreSqlContainer,
   StartedPostgreSqlContainer,
-} from "@testcontainers/postgresql";
-import { Client } from "pg";
+} from '@testcontainers/postgresql';
+import { Client } from 'pg';
 
 // FIX: https://github.com/kulshekhar/ts-jest/issues/4127
 // import { setSchema, seedTypes } from "./db_seed.js";
@@ -12,16 +12,16 @@ declare global {
   var __TESTCONTAINER__: StartedPostgreSqlContainer;
 }
 
-export default async () => {
-  console.log("Global Setup");
+export async function setup() {
+  console.log('Global Setup');
 
   // CONSIDER: setting env variables here instead of passing .env.test
   // CONSIDER: staging an auth token
 
-  console.log("Starting PostgreSQL container...");
+  console.log('Starting PostgreSQL container...');
 
   // TODO: confirm pg version
-  const container = await new PostgreSqlContainer("postgres:latest")
+  const container = await new PostgreSqlContainer('postgres:latest')
     .withDatabase(process.env.POSTGRES_DB!)
     .withUsername(process.env.POSTGRES_USER!)
     .withPassword(process.env.POSTGRES_PASSWORD!)
@@ -29,17 +29,17 @@ export default async () => {
 
   // CONSIDER: audit container details
 
-  console.log("PostgreSQL container started.");
+  console.log('PostgreSQL container started.');
 
   // After the container starts, its port and other details are dynamically assigned.
   // We need to overwrite the environment variables with these real connection parameters
   // so that the application can connect to the correct database instance during tests.
   Object.assign(process.env, {
-    POSTGRES_HOST: container.getHost() == "localhost" ? "0.0.0.0" : null, // causes a 57P01 error is host is 'localhost'
+    POSTGRES_HOST: container.getHost() == 'localhost' ? '0.0.0.0' : null, // causes a 57P01 error is host is 'localhost'
     POSTGRES_PORT: container.getPort().toString(),
   });
 
-  console.log("Running database migrations and seeding...");
+  console.log('Running database migrations and seeding...');
 
   // establish a database connection
   const client = new Client({
@@ -54,45 +54,45 @@ export default async () => {
 
   // define database schema
   for (const tableGroup of [
-    "type_tables",
-    "primary_tables",
-    "user_tables",
-    "event_tables",
-    "review_tables",
-    "relationship_tables",
+    'type_tables',
+    'primary_tables',
+    'user_tables',
+    'event_tables',
+    'review_tables',
+    'relationship_tables',
   ]) {
     const sqlText = await fs.readFile(
       `./sql/tables/${tableGroup}.sql`,
-      "utf-8",
+      'utf-8'
     );
     await client.query(sqlText);
   }
 
   // populate with type data
   for (const typePair of [
-    { table: "artist_types", jsonFile: "artists_types.data.json" },
-    { table: "venue_types", jsonFile: "venues_types.data.json" },
-    { table: "series_types", jsonFile: "series_types.data.json" },
-    { table: "event_types", jsonFile: "events_types.data.json" },
+    { table: 'artist_types', jsonFile: 'artists_types.data.json' },
+    { table: 'venue_types', jsonFile: 'venues_types.data.json' },
+    { table: 'series_types', jsonFile: 'series_types.data.json' },
+    { table: 'event_types', jsonFile: 'events_types.data.json' },
     {
-      table: "hash_algorithm_types",
-      jsonFile: "hash_algorithms_types.data.json",
+      table: 'hash_algorithm_types',
+      jsonFile: 'hash_algorithms_types.data.json',
     },
-    { table: "role_types", jsonFile: "roles_types.data.json" },
-    { table: "relationship_types", jsonFile: "relationships_types.data.json" },
-    { table: "genre_types", jsonFile: "genres_types.data.json" },
+    { table: 'role_types', jsonFile: 'roles_types.data.json' },
+    { table: 'relationship_types', jsonFile: 'relationships_types.data.json' },
+    { table: 'genre_types', jsonFile: 'genres_types.data.json' },
     //   { table: "subgenres_types", jsonFile: "subgenres_types.data.json" }
   ]) {
     const jsonData: { type_description: string; type_id: number }[] =
       JSON.parse(
-        await fs.readFile(`./db_seed/data/types/${typePair.jsonFile}`, "utf-8"),
+        await fs.readFile(`./db_seed/data/types/${typePair.jsonFile}`, 'utf-8')
       );
     for (const item of jsonData) {
       const values = [item.type_description, item.type_id];
-      // FIX: possibly prone to sql injection
+      // FIX: prone to sql injection
       await client.query(
         `INSERT INTO ${typePair.table} (type_description, type_id) VALUES ($1, $2);`,
-        values,
+        values
       );
     }
   }
@@ -101,8 +101,19 @@ export default async () => {
 
   client.end();
 
-  console.log("Database is ready.");
+  console.log('Database is ready.');
 
   // Store the container instance in a global variable to access it in the teardown script
   globalThis.__TESTCONTAINER__ = container;
-};
+}
+
+export async function teardown() {
+  console.log('\nTearing down test environment...');
+
+  const container: StartedPostgreSqlContainer = globalThis.__TESTCONTAINER__;
+
+  if (container) {
+    await container.stop();
+    console.log('Container stopped successfully.');
+  }
+}
