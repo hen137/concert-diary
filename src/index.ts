8cd44f3257adde7782ebec62f8e6f75eb1156b68
Henry <henry.abramovich@gmail.com>
2026-05-02 13:31:37 -0400

    refactor: Move server opts out of factory func

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,24 @@
-import { buildServer } from "./server.js";
+import { buildServer } from './server.js';
 
-const server = buildServer();
+const server = buildServer({
+  fastifyOpts: {
+    // Fastify Server options: https://deepwiki.com/fastify/fastify/2.2-fastify-instance-api#core-properties
+    // TODO: expand server options
+    logger: {
+      level: process.env.LOG_LEVEL!,
+      transport: {
+        target: 'pino-pretty',
+        options: {
+          colorize: true,
+          translateTime: 'HH:MM:ss Z',
+        },
+      },
+    },
+    genReqId(_req) {
+      return Math.random().toString(36).substring(2, 12); // generates a random 10 character string ensuring unique request ids
+    },
+  },
+});
 
 // TODO: improve server type inference
 export type Server = typeof server;
@@ -15,6 +33,6 @@ await server
 
   // })
   .catch((error) => {
-    console.error("Error starting server:", error);
+    console.error('Error starting server:', error);
     process.exit(1);
   });
--- src/server.ts
+++ src/server.ts
@@ -3,59 +3,51 @@ import type { ZodTypeProvider } from 'fastify-type-provider-zod';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 import Fastify from 'fastify';
+import type { FastifyServerOptions } from 'fastify';
 import autoLoad from '@fastify/autoload';
 import {
   serializerCompiler,
   validatorCompiler,
 } from 'fastify-type-provider-zod';
+// import type { BetterAuthOptions } from 'better-auth';
 
-// Fastify server factory function
-export function buildServer() {
-  const server = Fastify({
-    // Fastify Server options: https://deepwiki.com/fastify/fastify/2.2-fastify-instance-api#core-properties
-    // TODO: expand server options
-    logger: {
-      level: process.env.LOG_LEVEL!,
-      transport: {
-        target: 'pino-pretty',
-        options: {
-          colorize: true,
-          translateTime: 'HH:MM:ss Z',
-        },
-      },
-    },
-    genReqId(_req) {
-      return Math.random().toString(36).substring(2, 12); // generates a random 10 character string ensuring unique request ids
-    },
-  }).withTypeProvider<ZodTypeProvider>(); // uses ZodTypeProvider for schema validation, serialization and type inference in routes
+interface IServerOptions {
+  fastifyOpts: FastifyServerOptions;
+  // betterAuthOpts: BetterAuthOptions;
+}
+
+/**
+ * Fastify server factory function
+ * @returns
+ */
+export function buildServer(options: IServerOptions) {
+  const server = Fastify(
+    options.fastifyOpts
+  ).withTypeProvider<ZodTypeProvider>(); // uses ZodTypeProvider for schema validation, serialization and type inference in routes
 
   server.setValidatorCompiler(validatorCompiler);
   server.setSerializerCompiler(serializerCompiler);
 
   // Plugins
+
   server.register(autoLoad, {
     dir: join(dirname(fileURLToPath(import.meta.url)), 'plugins'),
     // matchFilter: (path) => path.includes("plugin"),
   });
 
   // Routes
+
   server
     .register(autoLoad, {
       dir: join(dirname(fileURLToPath(import.meta.url)), 'routes'),
       routeParams: true, // enable path paramaters
       dirNameRoutePrefix: true, // uses directory structure as route prefixes
-      ignorePattern: /auth.*/, // ignore auth routes so autohooks arent assigned to them
       autoHooks: true, // automatically register hooks from autohooks.ts found in dir
       cascadeHooks: true, // hooks registered in parent directories will be applied to child routes
     })
     .after((error) => {
-      if (error) server.log.error(error, 'Error registering routes:');
+      if (error) server.log.error(error, 'Error registering routes');
     });
-
-  server.register(autoLoad, {
-    dir: join(dirname(fileURLToPath(import.meta.url)), 'routes/v1/auth'),
-    dirNameRoutePrefix: true,
-  });
 
   //Decorators
 
