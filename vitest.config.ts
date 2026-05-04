623adae22da7919b20a44d29cf46c083152dbc0e
Henry Abramovich <henry.abramovich@gmail.com>
2026-05-04 19:43:54 -0400

    test: Omit global setup

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    globalSetup: './tests/utils/global_setup.ts',
+    // globalSetup: './tests/utils/global_setup.ts',
   },
 });
