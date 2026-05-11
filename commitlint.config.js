dcbf5d87d738982a991ea298fa7d38bc38eb4cb8
Henry <henry.abramovich@gmail.com>
2026-05-11 15:27:57 -0400

    chore(commit lint): Add chore type & restyling

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,53 +1,44 @@
 const config = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    // 1. Format: <type>([optional scope]): <description> - enforced by most rules below.
-    // Enforce that the type is not empty.
-    'type-empty': [2, 'never'],
+    /**
+     * Format:
+     * <type>([optional scope]): <description>
+     *
+     * <body>
+     *
+     * <footer>
+     * */
 
-    // Enforce specific commit types. Add/remove types based on the project.
+    // Subject/Description Rules:
+    'type-empty': [2, 'never'], // enforce that the type is not empty
     'type-enum': [
       2,
       'always',
       [
         'feat',
         'fix',
-        'build',
-        'chore',
-        'ci',
-        'style',
-        'refactor',
-        'docs',
         'test',
         'perf',
+        'refactor',
+        'build',
+        'ci',
+        'chore',
+        'docs',
+        'style',
       ],
-    ],
-
-    // Subject/Description Rules:
-    // 2. Short and Summarized: Try to fit the subject line inside 50 characters.
-    'header-max-length': [2, 'always', 50],
-
-    // 3. Capitalize the description: Start subject line with a capital letter.
-    // Imperative Mood. Mostly useful for generating changelogs.
-    'subject-case': [2, 'always', 'sentence-case'],
-
-    // 4. Avoid trailing period.
-    'subject-full-stop': [2, 'never', '.'],
+    ], // enforce commit types
+    'scope-case': [2, 'always', 'lower-case'], // enforce that if a scope is used, it is in lower-case
+    'header-max-length': [2, 'always', 50], // fit the subject line inside 50 characters
+    'subject-case': [2, 'always', 'sentence-case'], // start subject line with a capital letter
+    'subject-full-stop': [2, 'never', '.'], // avoid trailing period
 
     // Body Rules:
-    // 5. Body is added by leaving a blank line after the subject line.
-    'body-leading-blank': [2, 'always'],
-
-    // 6. Wrap the body at 72 characters.
-    'body-max-line-length': [2, 'always', 72],
+    'body-leading-blank': [2, 'always'], // body is added by leaving a blank line after the subject line
+    'body-max-line-length': [2, 'always', 72], // wrap the body at 72 characters
 
     // Footer Rules:
-    // Ensure a blank line precedes the footer.
-    'footer-leading-blank': [2, 'always'],
-
-    // Rule for Optional Scope:
-    // Enforce that if a scope is used, it is in lower-case.
-    'scope-case': [2, 'always', 'lower-case'],
+    'footer-leading-blank': [2, 'always'], // ensure a blank line precedes the footer
   },
 };
 
